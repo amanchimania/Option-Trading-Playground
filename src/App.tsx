6fb8dbfe7d916a8d121bc8a2f0b44860d425cf9c
Aman Chimania <amanchimania@gmail.com>
2025-11-01 01:26:06 +0530

    created a socket connection with the web socket and used it in the App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './App.css'
+import useSocket from './socket/SocketConnection';
 
 function App() {
+  const { isReady, sendMessage, close } = useSocket();
+
+  useEffect(() => {
+    if (isReady) {
+      // sendMessage("Hello, server!");
+    }
+  }, [isReady, sendMessage]);
 
   return (
     <>
